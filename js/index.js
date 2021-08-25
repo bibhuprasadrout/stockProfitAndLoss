abd67822a5a25ed33be8d7d6d83259bc9ad89e19
bibhuprasadrout <bibhu.rs@gmail.com>
2021-08-25 23:52:27 +0530

    Style implemented and logic for button actions implemented

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const initialStockPrice = document.querySelector('.initial-stock-val')
 const currentStockPrice = document.querySelector('.current-stock-val')
 const stockCount = document.querySelector('.stock-count-val')
 const calculate = document.querySelector('.calculate')
+const reset = document.querySelector('.reset')
 const profitStatement = document.querySelector('.profit-statement')
 const lossStatement = document.querySelector('.loss-statement')
 const stagnantStatement = document.querySelector('.stagnant-statement')
@@ -12,15 +13,33 @@ function calculateProfitOrLoss(initialPrice, currentPrice, noOfStocks) {
   let loss = (initialPrice - currentPrice) * noOfStocks
   let lossPercentage = parseFloat((loss/initialPrice) * 100).toFixed(3)
   if(initialPrice > currentPrice) {
-    profitStatement.innerText = `At present you are at loss by ${loss}. You have accumulated a loss of ${lossPercentage}%`
-    profitStatement.classList.remove('hidden')
-    profitStatement.classList.add('visible')
-  } else if (currentPrice > initialPrice) {
-    lossStatement.innerText = `At present you are at profit by ${profit}. You have accumulated a profit of ${profitPercentage}%`
+    lossStatement.innerText = `At present you are at loss by ${loss}. You have accumulated a loss of ${lossPercentage}%`
+    document.querySelector('.input-all').classList.remove('visible')
+    document.querySelector('.input-all').classList.add('hidden')
+    profitStatement.classList.remove('visible')
+    profitStatement.classList.add('hidden')
+    stagnantStatement.classList.remove('visible')
+    stagnantStatement.classList.add('hidden')
     lossStatement.classList.remove('hidden')
     lossStatement.classList.add('visible')
+  } else if (currentPrice > initialPrice) {
+    profitStatement.innerText = `At present you are at profit by ${profit}. You have accumulated a profit of ${profitPercentage}%`
+    document.querySelector('.input-all').classList.remove('visible')
+    document.querySelector('.input-all').classList.add('hidden')
+    lossStatement.classList.remove('visible')
+    lossStatement.classList.add('hidden')
+    stagnantStatement.classList.remove('visible')
+    stagnantStatement.classList.add('hidden')
+    profitStatement.classList.remove('hidden')
+    profitStatement.classList.add('visible')
   } else {
     stagnantStatement.innerText = `At present your stocks show no sign of growth.`
+    document.querySelector('.input-all').classList.remove('visible')
+    document.querySelector('.input-all').classList.add('hidden')
+    lossStatement.classList.remove('visible')
+    lossStatement.classList.add('hidden')
+    profitStatement.classList.remove('visible')
+    profitStatement.classList.add('hidden')
     stagnantStatement.classList.remove('hidden')
     stagnantStatement.classList.add('visible')
   }
@@ -34,6 +53,17 @@ calculate.addEventListener('click', function() {
     document.querySelector('.input-all').innerText = `Please enter all relavent values`
     document.querySelector('.input-all').classList.remove('hidden')
     document.querySelector('.input-all').classList.add('visible')
-    document.querySelector('.input-all').classList.add('fill-all')
+    document.querySelector('.input-all').innerText = 'Please fill all details.'
   }
+})
+
+reset.addEventListener('click', function() {
+  document.querySelector('.profit-statement').classList.remove('visible')
+  document.querySelector('.profit-statement').classList.add('hidden')
+  document.querySelector('.loss-statement').classList.remove('visible')
+  document.querySelector('.loss-statement').classList.add('hidden')
+  document.querySelector('.stagnant-statement').classList.remove('visible')
+  document.querySelector('.stagnant-statement').classList.add('hidden')
+  document.querySelector('.input-all').classList.remove('visible')
+  document.querySelector('.input-all').classList.add('hidden')
 })
